cab1d1e041d586ab6f26885b704dc9727b874165
Carlos Augusto <c.augusto516@gmail.com>
2023-06-13 16:12:47 -0300

    fim da aula

---
+++ controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,14 @@
+public class ExemploForArray {
+    public static void main(String[] args) {
+
+    // array o primeiro índide é zero.
+	String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+
+
+    //alunos.length = tamanho do array
+	for (int x=0; x<alunos.length; x++) {
+		System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+	}
+
+}
+}
